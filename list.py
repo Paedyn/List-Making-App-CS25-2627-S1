9b15db5d6e34d31b427df6b03c6316ecaa95bfb8
Pavaki <pavaki190@gmail.com>
2026-09-10 08:48:04 -0600

    A

---
+++ list.py
@@ -0,0 +1,15 @@
+#
+# # for i in range(ask):
+#  ask=(input("Put an iten on your list here:"))
+#  print("Alright! Making you a list!")
+#     # list=[]
+#     # list.insert(ask)
+
+
+    # ask=(int(input("How many things would you like it the list?")))
+
+print("Hi! You can make a list here! Any type of list, I guess...")#TBD
+ask = (input("Put an item on your list here:"))
+list = []
+list.insert(0,ask)
+print(list)
