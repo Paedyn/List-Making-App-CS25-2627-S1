019b7a2d86f8fb74fee882ba29b2218480d962b2
Pavaki <pavaki190@gmail.com>
2026-09-14 08:49:27 -0600

    Update list.py

--- list.py
+++ list.py
@@ -5,7 +5,22 @@ list = []
 print("Hi! You can make a list here! Any type of list, I guess...")#TBD
 stuff=(int(input("How many tasks would you like?")))
 
-for i in range(stuff):
+for _ in range(stuff):
     task=(input("List your tasks here!"))
 list.append(task)
 print(list)
+more_stuff=(input("Write here:"))
+list.insert[0,more_stuff]
+
+
+# ##Add at least 3 items.
+# Insert an item somewhere other than the end.
+# Change an existing item.
+# Search for an item that exists.
+# Search for an item that does not exist.
+# Remove an item.
+# Display all current items.
+# Display the current number of items.
+# Undo a change.
+# Handle at least one invalid position without crashing.
+# Exit normally.
